198e4df0685f160234ab809b397615921e3ac6d4
Alessandro Astone <ales.astone@gmail.com>
2020-10-29 08:40:54 +0100

    ido: Define a soong namespace
    Change-Id: Ida759ee1786c667e766da29103f0ecb54950c104

--- device.mk
+++ device.mk
@@ -141,6 +141,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service.msm8916 \
     sensors.msm8916
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/_hals.conf:system/vendor/etc/sensors/_hals.conf \
     $(LOCAL_PATH)/configs/calmodule.cfg:system/vendor/etc/calmodule.cfg
