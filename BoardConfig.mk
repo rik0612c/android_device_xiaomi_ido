741a4b3134eade609bf131435a0ca491fd562dc6
rik0612c <qq872638399@gmail.com>
2023-10-26 22:59:29 +0800

    ido: BoardConfig: Fake vendor security patch level

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,6 +78,9 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 BOARD_NO_SECURE_DISCARD := true
 
+# Security patch level
+VENDOR_SECURITY_PATCH := 2023-09-01
+
 # SELinux
 BOARD_SEPOLICY_DIRS += \
     $(DEVICE_PATH)/sepolicy
